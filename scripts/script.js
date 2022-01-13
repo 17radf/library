b237be242736c497cc428f290f0128b2d6371911
Raihan <12584890+17radf@users.noreply.github.com>
2022-01-13 17:47:30 +0700

    refresher, 3 times lmao

--- scripts/script.js
+++ scripts/script.js
@@ -1,108 +1,95 @@
-// initiate empty array for storing books and book constructor
-let library = [];
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
+// initialize library variable
+let library = []
+
+// book's object
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.isRead = () => this.read ? "✅" : "❌"
 }
 
-// functions for adding books to library and delete
-function addBookToLibrary(title, author, pages, isRead) {
-  const newBook = new Book(title, author, pages, isRead);
-  const newArray = library.push(newBook);
-  library.concat(newArray);
+Book.prototype.setRead = function() {
+  this.read = !this.read
+  display()
+}
+
+// function to add book to library
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
+  library.push(newBook)
 }
 
 function deleteBook(index) {
-  library.splice(index, 1);
-  display();
+  library.splice(index, 1)
+  display()
 }
 
-function readBook(index) {
-  library[index].isRead = !library[index].isRead;
-  display();
-}
-
-// queries selector
-const container = document.querySelector(".container");
-const containerBook = document.querySelector(".containerBooks");
-const btnSub = document.querySelector("#btnSubmit");
-const div = document.createElement("div");
-
-// function for displaying books dom
+// function to display any books (it loops everything in variable => library)
 function display() {
-  containerBook.innerHTML = "";
-  for (let i = 0; i < library.length; i++) {
-    const divider = document.createElement("div");
-    const header = document.createElement("h3");
-    const text = document.createElement("p");
-    const title = document.createElement("span");
-    const author = document.createElement("span");
-    const pages = document.createElement("span");
-    const delBtn = document.createElement("button");
-    const readBtn = document.createElement("button");
 
-    header.textContent = `Book ${i + 1}`;
-    title.textContent = `${library[i].title} `;
-    author.textContent = `by ${library[i].author} `;
-    pages.textContent = `with ${library[i].pages} pages `;
-    readBtn.textContent = `${library[i].isRead ? "Read" : "Not read yet"}`;
-    delBtn.textContent = `Delete`;
+  const books = document.querySelector("div.books")
+  books.innerHTML = null
 
-    delBtn.addEventListener("click", () => {
-      deleteBook(i);
-    });
-    readBtn.addEventListener("click", () => {
-      readBook(i);
-    });
+  for(let i = 0; i < library.length; i++) {
+    // declare variables to create
+    const container = document.createElement("div")
+    const title = document.createElement("h3")
+    const author = document.createElement("p")
+    const divBtn = document.createElement("div")
+    const delBtn = document.createElement("button")
+    const readBtn = document.createElement("button")
 
-    divider.classList.add(`book-seperator`);
-    divider.classList.add(`book-${i}`);
-    title.classList.add("title-book");
-    author.classList.add("author-book");
-    pages.classList.add("pages-book");
-    readBtn.classList.add("btn-book");
-    readBtn.classList.add("green");
-    delBtn.classList.add("btn-book");
-    delBtn.classList.add("red");
+    // umm... the contents
+    container.classList.add("book")
+    divBtn.classList.add("div-btn")
 
-    containerBook.appendChild(divider);
-    divider.appendChild(header);
-    divider.appendChild(text);
-    text.appendChild(title);
-    text.appendChild(author);
-    text.appendChild(pages);
-    divider.appendChild(readBtn);
-    divider.appendChild(delBtn);
+    title.textContent = `${library[i].title} ${library[i].isRead()}`
+    author.textContent = `by ${library[i].author}, ${library[i].pages} pages`
+    delBtn.textContent = `DELETE`
+    readBtn.textContent = `READ`
+
+    // more advanced contents
+    delBtn.addEventListener("click", () => deleteBook(i))
+    readBtn.addEventListener("click", () => library[i].setRead())
+
+    // apply it
+    container.appendChild(title)
+    container.appendChild(author)
+    container.appendChild(divBtn)
+    divBtn.appendChild(delBtn)
+    divBtn.appendChild(readBtn)
+    books.appendChild(container)
+
   }
-  localStorage.setItem("library", JSON.stringify(library));
-  return;
 }
 
-// button for submit
-btnSub.addEventListener("click", () => {
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const isRead = document.querySelector("#isRead").value;
-  title && author && pages && isRead
-    ? addBookToLibrary(title, author, JSON.parse(pages), JSON.parse(isRead))
-    : alert("isi semua dong");
-  display();
-});
+const submit = document.querySelector("#btn-submit")
+submit.addEventListener("click", (e) => {
+  const title = document.querySelector("#title").value
+  const author = document.querySelector("#author").value
+  const pages = document.querySelector("#pages").value
+  const isRead = document.querySelector("#isRead").checked
+  console.log(title, pages, author, isRead)
+  title && pages && author ? addBookToLibrary(title, author, pages, isRead) : alert("Yang lengkap dong mz")
+  display()
+})
 
-// check if there is localstorage and then call display
-if (localStorage.getItem("library") !== null) {
-  library = JSON.parse(localStorage.getItem("library"));
-} else {
-  addBookToLibrary(
-    "The Adventure Life of Pengamuk Gandi",
-    "Gandi Himself",
-    69,
-    false
-  );
-  addBookToLibrary("Pesindir Sedjati", "Dian", 420, false);
+const showBtn = document.querySelector("#btn-show")
+showBtn.addEventListener("click", () => toggleForm())
+
+const backBtn = document.querySelector("#btn-back")
+backBtn.addEventListener("click", () => toggleForm())
+
+const toggleForm = () => {
+  const formSection = document.querySelector(".form")
+  formSection.classList.toggle("disable")
+  showBtn.classList.toggle("disable")
 }
-display();
 
+addBookToLibrary("Tutorial Ngamuk di Depan Murid", "Gandi", 69, true)
+addBookToLibrary("Cara Jitu Menyindir", "Dian", 20, false)
+addBookToLibrary("The Man", "Not Raihan", 10, false)
+
+display()
